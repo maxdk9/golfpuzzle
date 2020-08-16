c2b7994a54356675b38d1391173cace1a075bf96
maxdk9 <maxdk9@gmail.com>
2020-08-16 12:40:15 +0300

    largefileproblem

---
+++ golfpuzzle/Assets/FollowTrail.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FollowTrail : MonoBehaviour
+{
+    
+    private TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         // Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         // transform.position = pos;
+
+    }
+
+    public void Move(Vector2 move)
+    {
+        StartCoroutine(MoveRoutine(move));
+    }
+
+    private IEnumerator MoveRoutine(Vector2 move)
+    {
+
+
+        float duration = .5f;
+        
+        
+        Vector3 destination = transform.position + (new Vector3(move.x,move.y,0) * 2);
+        this.transform.DOMove(destination, duration);
+        yield return new WaitForSeconds(duration);
+        
+    }
+}
---
+++ golfpuzzle/Assets/Scripts/MapObjects/MetalBlock.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using MapObjects;
+using UnityEngine;
+
+public class MetalBlock : iBall
+{
+    [SerializeField]
+    public float moveDuration = .05f;
+
+    public float waitForMoveDuration = .3f;
+    public Vector2 startPosition;
+
+    
+    
+    public override void Move(Vector2 direction)
+    {
+
+
+    
+        moved = true;
+        StartCoroutine(MoveBallRoutine(direction));
+    }
+
+    private IEnumerator MoveBallRoutine(Vector2 direction)
+    {
+        startPosition = this.transform.position;
+        moved = true;
+        while (moved)
+        {
+            yield return new WaitForSeconds(moveDuration);
+            moved = MoveBall(direction);    
+        }
+        yield return new WaitForSeconds(waitForMoveDuration);
+        ballStopped.Invoke();
+        
+    }
+    
+    
+    private bool MoveBall(Vector2 direction)
+    {
+        // if (Mathf.Abs(direction.x) < .5f)
+        // {
+        //     direction.x = 0;
+        // }
+        //
+        // if (Mathf.Abs(direction.y) < .5f)
+        // {
+        //     direction.y = 0;
+        // }
+        //
+        //
+        // direction.Normalize();
+        
+        
+        
+        if (Blocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            if(!this.isSunked){
+                transform.DOMove(transform.position + new Vector3(direction.x,direction.y), moveDuration);
+                return false;    
+            }
+        }
+
+        return false;
+    }
+    
+    
+    
+    
+
+    
+}
