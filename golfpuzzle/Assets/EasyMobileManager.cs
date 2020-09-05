c11aaac48dd4eafc86eec9a3f974febf81fee6e2
maxdk9 <maxdk9@gmail.com>
2020-09-05 22:14:49 +0300

    inapp purchase and ui improvements 2

---
+++ golfpuzzle/Assets/EasyMobileManager.cs
@@ -0,0 +1,206 @@
+﻿
+using System;
+using System.Collections;
+using Assets.SimpleLocalization;
+using common;
+using EasyMobile;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EasyMobileManager: Singleton<EasyMobileManager>
+    {
+        public bool AdvancedUnlocked = false;
+        public bool ProductChecked = false;
+        private GameObject progressBarInapp;
+        public static UnityEvent LevelPackPurchased=new UnityEvent();
+        
+        private void Start()
+        {
+            
+            
+        }
+
+        private void Awake()
+        {
+            if (!RuntimeManager.IsInitialized())
+                RuntimeManager.Init();
+            
+        }
+
+
+
+        public void RequestRating()
+        {
+
+            if (LevelResults.GetLastSolvedLevel()<30)
+            {
+                return;
+            }
+            
+            if (StoreReview.CanRequestRating())
+            {
+                
+                var localized = new RatingDialogContent(
+                    LocalizationManager.Localize("RATE_LOC_TITLE") +" "+ RatingDialogContent.PRODUCT_NAME_PLACEHOLDER,
+                    LocalizationManager.Localize("RATE_LOC_MESSAGE") +" "+ RatingDialogContent.PRODUCT_NAME_PLACEHOLDER + "?",
+                    LocalizationManager.Localize("RATE_LOC_LOWRATINGMESSAGE"),
+                    LocalizationManager.Localize("RATE_LOC_HIGHRATINGMESSAGE"),
+                    LocalizationManager.Localize("RATE_LOC_POSTPONEBUTTONLABEL"),
+                    LocalizationManager.Localize("RATE_LOC_REFUSEBUTTONLABEL"),
+                    LocalizationManager.Localize("RATE_LOC_RATEBUTTONLABEL"),
+                    LocalizationManager.Localize("RATE_LOC_CANCELBUTTONLABEL"),
+                    LocalizationManager.Localize("RATE_LOC_FEEDBACKBUTTONLABEL")
+                );
+                StoreReview.RequestRating(localized);    
+            }
+            
+            
+        }
+        
+        
+        
+        
+
+        public void CheckProductsAvailability()
+        {
+            
+// #if UNITY_STANDALONE || UNITY_EDITOR
+//             AdvancedUnlocked = true;
+//             return;
+// #endif
+            bool isInitialized = InAppPurchasing.IsInitialized();
+            if (!isInitialized)
+            {
+                return;
+            }
+                        
+            AdvancedUnlocked = InAppPurchasing.IsProductOwned(EM_IAPConstants.Product_spacepuzzle_premiumlevels50_100);
+            
+            ProductChecked = true;
+
+        }
+
+        public void PurchaseAdvancedUnlocked()
+        {
+
+            if (!AdvancedUnlocked)
+            {
+                ProductChecked = false;
+                CheckProductsAvailability();
+                if (AdvancedUnlocked)
+                {
+                    return;
+                }
+            }
+            InAppPurchasing.Purchase(EM_IAPConstants.Product_spacepuzzle_premiumlevels50_100);
+        }
+
+
+        private void OnEnable()
+        {
+
+            InAppPurchasing.PurchaseCompleted += PurchaseCompletedHandle;
+            InAppPurchasing.PurchaseFailed += PurchaseFailedHandle;
+            
+            SceneMover.OnMainMenuLoadEvent.AddListener(RequestRating);
+            
+
+
+        }
+
+
+        private void Update()
+        {
+           
+
+            if (!ProductChecked)
+            {
+                CheckProductsAvailability();
+            }
+            else
+            {
+                if (progressBarInapp == null)
+                {
+                    progressBarInapp=GameObject.Find("ProgressBarInapp");
+                }
+                else
+                {
+                    progressBarInapp.SetActive(false);   
+                }
+            }
+            
+            
+        }
+
+       
+            
+        
+        
+
+
+
+
+        private void OnDisable()
+        {
+            InAppPurchasing.PurchaseCompleted -= PurchaseCompletedHandle;
+            InAppPurchasing.PurchaseFailed -= PurchaseFailedHandle;
+            
+
+        }
+
+        private void PurchaseFailedHandle(IAPProduct product)
+        {
+            NativeUI.Alert("ERROR","Purchase failed " + product.Name);
+        }
+
+        private void PurchaseCompletedHandle(IAPProduct product)
+        {
+            switch (product.Name)
+            
+            {
+                   case EM_IAPConstants.Product_spacepuzzle_premiumlevels50_100:
+                       AdvancedUnlocked = true;
+                       LevelPackPurchased.Invoke();
+                       break;
+                   default:
+                       break;
+            }
+        }
+
+        public bool UserSpendMoney()
+        {
+            return AdvancedUnlocked;
+        }
+
+
+        public void ShareAppLink()
+        {
+
+            String appUrl = "https://play.google.com/store/apps/details?id=mazzy.and.thirtyrails";
+            
+            
+#if UNITY_IOS
+        appUrl="";
+ 
+#elif UNITY_ANDROID
+            appUrl = "https://play.google.com/store/apps/details?id=mazzy.and.spacepuzzle";
+#endif
+            // Share a URL
+            Sharing.ShareURL(appUrl);
+        }
+
+
+        public void ShareScreenshot()
+        {
+            this.StartCoroutine(SaveScreenshot());
+            string path = System.IO.Path.Combine(Application.persistentDataPath, "screenshot");
+            //Sharing.ShareImage(path, "This is a sample message");
+            Sharing.ShareScreenshot("screenshot_spacepuzzle", "space puzzle ");
+        }
+        
+        IEnumerator SaveScreenshot()
+        {
+            yield return new WaitForEndOfFrame();
+            string path = Sharing.SaveScreenshot("screenshot");
+        }
+    }
---
+++ golfpuzzle/Assets/Scripts/Tools/GolfTools.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class GolfTools : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void RenameFiles()
+    {
+
+        string olddirectory = @"D:\1\modify";
+        DirectoryInfo d = new DirectoryInfo(olddirectory);
+
+        string newdirectory = @"D:\1\new";
+        FileInfo[] infos = d.GetFiles();
+        foreach(FileInfo f in infos)
+        {
+            //File.Move(f.FullName, f.FullName.Replace("abc_",""));
+            string filename = f.Name;
+            string extension = f.Extension;
+            
+            
+            string filename1=filename.Replace( extension, "");
+            
+            int filenumber=Int32.Parse(filename1);
+            filenumber = filenumber + 1;
+
+            string newname = filenumber.ToString() + extension;
+
+            string newfullname = f.FullName.Replace(olddirectory, newdirectory);
+            newfullname = newfullname.Replace(filename, newname);
+            
+            
+            File.Copy(f.FullName,newfullname);
+            
+            //Debug.Log(String.Format("Old name : {0} ; new name :{1}",f.FullName,newfullname));
+            
+            
+        }
+    }
+
+
+    public void CorrectJson()
+    {
+        
+        String filename = "cards1.json";
+        string filePath = GetStreamingAssetsFilePath.GetPath(filename);
+        string dataAsJson="";
+        List<Level> mLevels=new List<Level>();
+        if (File.Exists(filePath))
+        {
+            dataAsJson = File.ReadAllText(filePath);
+            
+         
+            mLevels = JsonConvert.DeserializeObject<List<Level>>(dataAsJson,
+                new Newtonsoft.Json.Converters.StringEnumConverter());
+        }
+        else
+        {
+            Debug.Log(filename + "No file exist");
+        }
+
+        foreach (Level level in mLevels)
+        {
+            level.mMapPath = "LevelStorage/" + level.levelNumber.ToString();
+        }
+
+        String result = JsonConvert.SerializeObject(mLevels);
+        Debug.Log(result);
+
+
+    }
+    
+    
+
+    public void ShowMessageWindow()
+    {
+        this.StartCoroutine(ShowOrHideRoutine());
+
+
+    }
+
+    private IEnumerator ShowOrHideRoutine()
+    {
+        UIManager uiManager = GameObject.FindObjectOfType<UIManager>();
+
+        if (uiManager.tutorialMessageWindow.showed)
+        {
+            uiManager.tutorialMessageWindow.Hide();
+            
+        }
+        else
+        {
+            uiManager.tutorialMessageWindow.Show();    
+        }
+        
+        
+
+        yield return null;
+    }
+}
