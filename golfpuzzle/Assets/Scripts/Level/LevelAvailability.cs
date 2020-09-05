abd835eec2a6c63c8eb225a275f9d3703781737a
maxdk9 <maxdk9@gmail.com>
2020-09-05 22:10:44 +0300

    inapp purchase and ui imporvements

---
+++ golfpuzzle/Assets/Scripts/Level/LevelAvailability.cs
@@ -0,0 +1,20 @@
+
+    public class LevelPurchase
+    {
+        public static bool LevelPurchased(int levelNumber)
+        {
+            if (levelNumber <= 50)
+            {
+                return true;
+            }
+
+            if (EasyMobileManager.Instance.AdvancedUnlocked)
+            {
+                if (levelNumber <= 100)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
